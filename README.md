33a40b344215a00970b1226fb61d75970a4bcada
Andrew Rothstein <andrew.rothstein@gmail.com>
2019-10-06 15:50:07 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,39 @@
+andrewrothstein.nats_server
+=========
+[![Build Status](https://travis-ci.org/andrewrothstein/ansible-nats_server.svg?branch=master)](https://travis-ci.org/andrewrothstein/ansible-nats_server)
+
+Installs [nats-server](https://github.com/nats-io/nats-server).
+
+Requirements
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Role Variables
+--------------
+
+See [defaults/main.yml](defaults/main.yml)
+
+Dependencies
+------------
+
+See [meta/main.yml](meta/main.yml)
+
+Example Playbook
+----------------
+
+```yml
+- hosts: servers
+  roles:
+    - andrewrothstein.nats_server
+```
+
+License
+-------
+
+MIT
+
+Author Information
+------------------
+
+Andrew Rothstein <andrew.rothstein@gmail.com>
---
+++ dl-checksum.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env sh
+DIR=~/Downloads
+
+dl()
+{
+    local app=$1
+    local ver=$2
+    local os=$3
+    local arch=$4
+    local platform="${os}-${arch}"
+    local mirror=https://github.com/nats-io/${app}/releases/download/${ver}
+    local file=${app}-${ver}-${platform}.zip
+    local lfile=$DIR/$file
+    local url=$mirror/$file
+    if [ ! -e $lfile ];
+    then
+        wget -q -O $lfile $url
+    fi
+
+    printf "        # %s\n" $url
+    printf "        %s: sha256:%s\n" $platform $(sha256sum $lfile | awk '{print $1}')
+}
+
+dlapp() {
+    local app=$1
+    local ver=$2
+    printf "      %s:\n" $ver
+    dl $app $ver darwin amd64
+    dl $app $ver linux 386
+    dl $app $ver linux amd64
+    dl $app $ver linux arm6
+    dl $app $ver linux arm64
+    dl $app $ver linux arm7
+    dl $app $ver windows 386
+    dl $app $ver windows amd64
+}
+
+dlapp nats-server ${1:-v2.1.0}
+
